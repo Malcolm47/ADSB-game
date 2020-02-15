884656c6ccd468831049ef4712b080c4f5bd024f
Malcolm <malcolmmaas@gmail.com>
2020-02-15 15:51:43 -0500

    tilemap, movement, etc

---
+++ assets/testing.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="testing" tilewidth="32" tileheight="32" tilecount="22" columns="11">
+ <image source="momement.png" width="365" height="78"/>
+</tileset>
