77fde3ea00eef00c54e2dc012d181975a371262d
Malcolm <malcolmmaas@gmail.com>
2020-02-15 11:46:35 -0500

    added anim test

--- src/main.js
+++ src/main.js
@@ -7,7 +7,8 @@ var config = {
         },
         scene: {
             preload: preload,
-            create: create
+            create: create,
+            update: update
         }
     };
 
@@ -18,15 +19,32 @@ var config = {
     // IMPORTANT! Web server needs to be running for ADSB-game folder.
     // index.html can be run via IDE but the image paths need to be through server.
     // Make sure "set CORS headers" option is on
-        this.load.image('test', 'http://127.0.0.1:8887/assets/momement.png');
+        this.load.image('bruh', 'http://127.0.0.1:8887/assets/momement.png');
+        this.load.spritesheet('bert', 'http://127.0.0.1:8887/assets/bert.png', {
+            frameWidth: 107,
+            frameHeight: 140
+        });
     }
 
     function create ()
     {
-
-        var test = this.physics.add.image(400, 100, 'test');
+        var test = this.physics.add.image(400, 100, 'bruh');
+        this.bert = this.add.sprite(200,200,'bert');
 
         test.setVelocity(100, 200);
         test.setBounce(1, 1);
         test.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'bert_anim',
+            frames: this.anims.generateFrameNumbers('bert'),
+            frameRate: 10,
+            repeat: -1
+        });
+
+    }
+
+    function update ()
+    {
+        this.bert.play('bert_anim', true);
     }
