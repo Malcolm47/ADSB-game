d1c0bba2b9e1afd14d5b83555bb752c2c2c00569
Malcolm <malcolmmaas@gmail.com>
2020-05-22 15:07:15 -0400

    Sprint 2 - more enemies, character anim

--- src/play.js
+++ src/play.js
@@ -27,7 +27,6 @@ class play extends Phaser.Scene {
             frameWidth: 35,
             frameHeight: 50
         });
-        // REMEMBER TO ADD FLAG RESPONSIVENESS AT SOME POINT
         this.load.spritesheet('flag', '../assets/flag.png', {
             frameWidth: 25,
             frameHeight: 32
@@ -44,8 +43,8 @@ class play extends Phaser.Scene {
 
     create ()
     {
-        var spawnX = 3035;
-        var spawnY = 2220;
+        var spawnX = 110;
+        var spawnY = 2600;
         var score = 0;
 
         var coinX = [1220,1290,1150, 338,2151,2491,3231,3521,3682,3938,3772,3682,3592,3220,3300,1820,1627,1370,1181,1025,
@@ -54,6 +53,9 @@ class play extends Phaser.Scene {
         var coinY = [2550,2550,2550,2614,2422,2294,1969,1846,1718,1334,1078,1078,1078, 950, 950,1078,1398,1782,2102,2166,
         2166,2230,2230,2230,2230,2230,2230,2230,2230,2225,2225,2225,2161,2161,2353,2289,2161,2033,2033,2033,2033,2033,2033,
         2033,2033,2033,2737,2737,2737,2737,1073, 945, 817, 817, 881, 945, 945, 945,1201, 881, 753, 753, 753, 753];
+
+        var enemyX = [ 600,1200,4800,5400,6100,4100,4700,6500,5500,2900];
+        var enemyY = [2500,2400, 700, 800, 850,2100,2200,2000,2300, 800];
 
         const map = this.make.tilemap({ key: 'map' });
         // first thing is embedded tileset name, second is tileset png
@@ -90,16 +92,26 @@ class play extends Phaser.Scene {
 
         this.enemies = this.physics.add.group({
             immovable: true,
-            moves: true
+            moves: true,
+            key: 'enemy'
         });
-        this.enemies.create(600, 2550, 'enemy');
-        this.enemies.create(1200, 2400, 'enemy');
+        enemyX.forEach(createEnemies, this);
+        function createEnemies(value, index) {
+            this.enemies.create(value, enemyY[index]);
+            console.log(value)
+        }
+        var asdf = 0;
         this.enemies.children.iterate((child) => {
             child.setSize(45,10);
             child.setOffset(0,4)
             child.setScale(3);
             child.setVelocityX(160);
             child.setBounceX(1);
+            // This is really bad but I have no clue why it's creating 
+            // a weird duplicate
+            if (child.y == 0) {
+                child.disableBody(true,true);
+            }
         });
 
         this.bert = this.physics.add.sprite(spawnX,spawnY,'bert').setScale(1.3);
@@ -127,8 +139,9 @@ class play extends Phaser.Scene {
         this.physics.add.collider(this.portal, platforms);
         this.physics.add.collider(this.checkpoints, [rear_platforms, platforms]);
         //this.physics.add.collider(this.checkpoints, platforms);
-        this.physics.add.collider(this.enemies, enemyWalls);
-        this.physics.add.collider(this.enemies, platforms);
+        this.physics.add.collider(this.enemies, [enemyWalls, platforms, rear_platforms]);
+        //this.physics.add.collider(this.enemies, platforms);
+        //this.physics.add.collider(this.enemies, rear_platforms);
         this.physics.add.collider(this.coins, [platforms, rear_platforms]);
         this.physics.add.collider(this.bert, goo, function(){
             this.bert.setX(spawnX);
@@ -167,9 +180,15 @@ class play extends Phaser.Scene {
         }, null, this);
 
         this.anims.create({
-            key: 'bert_anim',
-            frames: this.anims.generateFrameNumbers('bert'),
-            frameRate: 10,
+            key: 'bert_right',
+            frames: this.anims.generateFrameNumbers('bert', {start: 0, end: 8}),
+            frameRate: 30,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'bert_left',
+            frames: this.anims.generateFrameNumbers('bert', {start: 8, end: 0}),
+            frameRate: 30,
             repeat: -1
         });
         this.anims.create({
@@ -213,12 +232,13 @@ class play extends Phaser.Scene {
 
         keyC.on('up', function() {console.log(Math.floor(this.bert.body.position.x + 24) + ", " + 
         Math.floor(this.bert.body.position.y + 48));}, this)
-        keyP.on('up', function() {this.bert.setMaxVelocity(500,900)}, this)
+        keyP.on('up', function() {this.bert.setMaxVelocity(500,1100)}, this)
 
-        this.bert.setMaxVelocity(370, 900);
+        this.bert.setMaxVelocity(370, 1100);
 
         this.cameras.main.startFollow(this.bert);
         this.cameras.main.setLerp(0.1);
+        this.cameras.main.setBackgroundColor('#4488AA');
     }
 
     update ()
@@ -227,14 +247,24 @@ class play extends Phaser.Scene {
         this.bert.body.useDamping=true;
         this.bert.setDrag(0.9, 0); //takes value between 0-1 you can also set x,y seperately setDrag(x,y)
         if (this.keyW.isDown && this.bert.body.onFloor()) {
-            this.bert.setVelocityY(-450);
+            this.bert.setVelocityY(-500);
         }
         if (this.keyD.isDown) {
             this.bert.setAccelerationX(1000);
+            this.bert.play('bert_right', true);
         } else if (this.keyA.isDown) {
             this.bert.setAccelerationX(-1000);
+            this.bert.play('bert_left', true);
         } else {
             this.bert.setAccelerationX(0);
+            this.bert.anims.stop();
+            this.bert.setFrame(9);
+        }
+
+        if (this.bert.body.velocity.y > 0) {
+            this.bert.setFrame(11);
+        } else if (this.bert.body.velocity.y < 0) {
+            this.bert.setFrame(10);
         }
 
         this.enemies.children.iterate((child) => {
